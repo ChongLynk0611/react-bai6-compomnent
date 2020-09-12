19021c1b602fbc614c261c5d716c725e7e420a57
ChongLynk <tronglinhmta0611@gmail.com>
2020-09-12 10:19:36 -0400

    Them component headerLogin

---
+++ src/components/headerLogin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class headerLogin extends Component{
+    render(){
+        return(
+            <div className="headerLogin">
+                <p>hehe</p>
+            </div>
+        );
+    }
+}
+
+export default headerLogin;
---
+++ src/components/loginForm.js
@@ -0,0 +1,9 @@
+import React, {component} from 'react'
+
+class login extends component{
+    render(){
+        return(
+            
+        );
+    }
+}
